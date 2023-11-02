f5aa9a5c3c16a5a9c782dc1c8d6115990518c6df
Ram-1234 <ramnayan699@gmail.com>
2023-11-02 13:13:18 +0530

    add card feature

--- src/components/card/card.js
+++ src/components/card/card.js
@@ -1,13 +1,37 @@
-import React,{useContext} from 'react';
+import React, { useContext } from 'react';
 import { Context } from '../context';
 
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import VolunteerActivismIcon from '@mui/icons-material/VolunteerActivism';
+
 const Card = ({ src }) => {
-const theme = useContext(Context)
-console.log('theme', theme);
+    const theme = useContext(Context)
+    console.log('theme', theme);
+    const styles = { margin: "0", padding: "0", fontSize: "8px", fontFamily: "sans-serif", fontStyle: "oblique" }
     return (
         <div className="col mb-5">
             <div className="card" style={{ width: "18rem" }}>
-                <img src={src} className="card-img-top" alt="_card" />
+                <img src={src} className="card-img-top" style={{maxHeight:"290px"}} alt="_card" />
+                <div class="card-body bg-dark text-light" style={{ borderRadius: "0 0 5px 5px" }}>
+                    <p class="card-text">
+                        {/* <i class="bi bi-person-circle bg-light" style={{width:"50px",height:"50px", color:"white", background:"white"}}></i> */}
+                        <div className='user float-start'>
+                            <AccountCircleIcon />
+                            <p style={styles}>Uploaded</p>
+                        </div>
+                        <div className='likes float-end d-flex' style={styles}>
+                            <div className='support m-1 p-0'>
+                                <VolunteerActivismIcon />
+                                <p style={styles}>Support</p>
+                            </div>
+                            <div className='like m-1 p-0'>
+                                <FavoriteIcon />
+                                <p style={styles}>Likes</p>
+                            </div>
+                        </div>
+                    </p>
+                </div>
             </div>
         </div>
     )
